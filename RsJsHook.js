706776ba5d66986189cab1fcebc487b8ec44cd45
诸葛村夫 <41735199+GangPeter@users.noreply.github.com>
2023-02-22 01:37:24 +0800

    更新 RsJsHook.js

--- RsJsHook.js
+++ RsJsHook.js
@@ -1,10 +1,10 @@
 var body = $response.body
     //替换无感支付
-    /*.replaceAll("0==i.myUserInfo.authpost","0==1")
+    .replaceAll("0==i.myUserInfo.authpost","0==1")
     .replaceAll("0!=this.myUserInfo.authpost","0!=1")
     .replaceAll("0!=t.myUserInfo.authpost","0!=1")
     .replaceAll("0==this.myUserInfo.authpost","0==1")
-    .replaceAll("0==i.myUserInfo.authpost","0==1")*/
+    .replaceAll("0==i.myUserInfo.authpost","0==1")
     //替换报错提醒
     .replaceAll("enableDebug: !1", "enableDebug: 1")
     //替换页面转跳
