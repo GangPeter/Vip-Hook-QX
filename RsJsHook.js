2c1964354f1b67eae3e15d9a9f96b4b5cca50078
诸葛村夫 <41735199+GangPeter@users.noreply.github.com>
2023-02-22 01:30:47 +0800

    更新 RsJsHook.js

--- RsJsHook.js
+++ RsJsHook.js
@@ -6,9 +6,9 @@ var body = $response.body
     .replaceAll("0==this.myUserInfo.authpost","0==1")
     .replaceAll("0==i.myUserInfo.authpost","0==1")*/
     //替换报错提醒
-    //.replaceAll("S!=w.iOrderID", "S!=S")
+    .replaceAll("enableDebug: !1", "enableDebug: 1")
     //替换页面转跳
-    .replaceAll("url:\"../cz/cz\"", "this.func_toast(uni.getStorageSync(\"ISPDATA\"))")
+    //.replaceAll("url:\"../cz/cz\"", "this.func_toast(uni.getStorageSync(\"ISPDATA\"))")
     //替换弹窗
     //.replaceAll("(console.log(\"接收数据：\",o)", "(console.log(\"接收数据：\",o),uni.showToast({title:\"设备4连接\"+o,icon:\"none\",duration:2e3}),")
 $done({ body });
