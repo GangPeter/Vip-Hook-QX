bfab289a21d70a031b2244c7d11f6ba13ee369d7
GangPeter <1411989855@qq.com>
2023-02-21 15:29:56 +0800

    1

--- RsJsHook.js
+++ RsJsHook.js
@@ -6,7 +6,7 @@ var body = $response.body
     .replaceAll("0==this.myUserInfo.authpost","0==1")
     .replaceAll("0==i.myUserInfo.authpost","0==1")*/
     //替换报错提醒
-    .replaceAll("S!=w.iOrderID", "S!=S")
+    //.replaceAll("S!=w.iOrderID", "S!=S")
     //替换页面转跳
     .replaceAll("t.confirm?i.func_Apply():i.func_closewg()", "uni.navigateTo({url:\"../grxx/grxx\"})")
     //替换弹窗
