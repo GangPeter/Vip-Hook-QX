ee8b50bf28af94c9aa6402f00a40f2063eca6ce4
GangPeter <1411989855@qq.com>
2023-02-21 15:15:02 +0800

    1

--- RsJsHook.js
+++ RsJsHook.js
@@ -1,12 +1,14 @@
 var body = $response.body
-//替换无感支付
-/*.replaceAll("0==i.myUserInfo.authpost","0==1")
-.replaceAll("0!=this.myUserInfo.authpost","0!=1")
-.replaceAll("0!=t.myUserInfo.authpost","0!=1")
-.replaceAll("0==this.myUserInfo.authpost","0==1")
-.replaceAll("0==i.myUserInfo.authpost","0==1")*/
-//替换报错提醒
-.replaceAll("S!=w.iOrderID","S!=S")
-//替换页面转跳
-.replaceAll("t.confirm?i.func_Apply():i.func_closewg()","uni.navigateTo({url:\"../grxx/grxx\"})")
+    //替换无感支付
+    /*.replaceAll("0==i.myUserInfo.authpost","0==1")
+    .replaceAll("0!=this.myUserInfo.authpost","0!=1")
+    .replaceAll("0!=t.myUserInfo.authpost","0!=1")
+    .replaceAll("0==this.myUserInfo.authpost","0==1")
+    .replaceAll("0==i.myUserInfo.authpost","0==1")*/
+    //替换报错提醒
+    .replaceAll("S!=w.iOrderID", "S!=S")
+    //替换页面转跳
+    .replaceAll("t.confirm?i.func_Apply():i.func_closewg()", "uni.navigateTo({url:\"../grxx/grxx\"})")
+    //替换弹窗
+    .replaceAll("then((function(o){", "then((function(o){Promise.reject(o),")
 $done({ body });
