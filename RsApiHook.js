76e9f927fa9438da9d3e2dfe6804e0e02a1286ae
诸葛村夫 <41735199+GangPeter@users.noreply.github.com>
2023-02-22 23:36:20 +0800

    更新 RsApiHook.js

--- RsApiHook.js
+++ RsApiHook.js
@@ -3,6 +3,7 @@ var body = $response.body
 	//.replaceAll("authpost\":0", "authpost\":0")
 	//金额
 	.replaceAll("accmoneyr\":-", "accmoneyr\":100000")
+    	.replaceAll("accmoneyr\":\"-", "accmoneyr\":\"100000")
 	//赠送金额
 	.replaceAll("accmoneyg\":-", "accmoneyg\":100000")
 	//orderid
@@ -11,6 +12,6 @@ var body = $response.body
 	//.replaceAll("xfcmd\":\"\"", "xfcmd\":\"06001C8A5867AF3D9D8A5762E73C9B950F62E75E9C4C89C5DAB0B48A5762E74E\"")
 	//.replaceAll("rsncmd\":\"\"", "rsncmd\":\"0700088A5867AF63A86CDB88\"")
 	//错误代码
-	.replaceAll("error_code\":-2", "error_code\":0")
-	.replaceAll("error_code\":\"-2\"", "error_code\":\"0\"")
+	//.replaceAll("error_code\":-2", "error_code\":0")
+	//.replaceAll("error_code\":\"-2\"", "error_code\":\"0\"")
 $done({ body });
